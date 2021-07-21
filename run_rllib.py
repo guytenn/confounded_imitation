dc5474f90658c71afab8a1609fddd93d8e6ffaf2
Guy <guytenn@gmail.com>
2021-07-21 16:06:55 +0300

    change slateq parameters

--- run_rllib.py
+++ run_rllib.py
@@ -48,7 +48,7 @@ def setup_config(env, algo, dice_coef=0, no_context=False, n_confounders=-1, cov
         config = slateq.DEFAULT_CONFIG.copy()
         # config["hiddens"] = [100, 100]
         # config["train_batch_size"] = 128
-        config["slateq_strategy"] = 'QL'
+        config["slateq_strategy"] = 'SARSA'
 
     config['wandb_logger'] = wandb_logger
     if covariate_shift:
