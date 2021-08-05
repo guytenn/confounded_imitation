8bcb96f1115f4477ecebbaf959fb0e20f362b076
guy <guytenn@gmail.com>
2021-08-05 08:58:02 +0300

    sparse reward to make things harder

--- src/envs/recsim_extension/interest_evolution.py
+++ src/envs/recsim_extension/interest_evolution.py
@@ -714,7 +714,7 @@ def clicked_watchtime_reward(responses, user_obs=None, doc_obs=None):
   reward = 0.0
   # reward = np.tanh(REWARD_MATRIX[0] @ (np.cos(REWARD_MATRIX @ user_obs / 30) * np.sin(REWARD_MATRIX @ doc_obs[0] / 30)) / 10)
   # print(reward)
-  reward = np.sum(user_obs @ doc_obs.T)
+  reward = (np.random.rand() < 0.05) * np.sum(user_obs @ doc_obs.T)
   # mat = REWARD_MATRIX[0:len(user_obs), 0:len(user_obs)]
   # reward = np.sum(user_obs @ mat @ doc_obs.T) / 10
   for response in responses:
