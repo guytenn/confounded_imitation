efe2e453599c10c96e447b80a7c21e52117d0d57
guy <guytenn@gmail.com>
2021-09-05 16:11:37 +0300

    seed

--- run_rllib.py
+++ run_rllib.py
@@ -120,7 +120,7 @@ def setup_config(env, args):
                     'confounding_strength': 0
                 }
         else:
-            config['env_config'] = {'sparse_reward': args.sparse, 'context_params': None}
+            config['env_config'] = {'seed': args.seed, 'sparse_reward': args.sparse, 'context_params': None}
     config['num_workers'] = num_processes
     # gpu_count = 1
     # num_gpus = 0.0001  # Driver GPU
